fe2cf436c4b077142994e668338038a0b04ba647
jennyq <jenny.q@me.com>
2021-08-18 11:14:41 -0400

    +semantic ui, axios, vue router, vuex, deleting default templates

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <header class="ui secondary pointing menu">
+        <a class="active item" href="/">
+            Image Storage
+        </a>
+        <div class="right menu">
+            <a class="ui item">Gallery</a>
+            <a class="ui item">Upload</a>
+            <a class="ui item">Log In</a>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
